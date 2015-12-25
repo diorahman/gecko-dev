161c7b9cc4c6048eef92d1ac9fbc393760242930
Dhi Aurrahman <diorahman@rockybars.com>
2015-12-25 07:57:44 +0700

    Add amanda branding patch-1

---
+++ browser/branding/amanda/configure.sh
@@ -0,0 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+MOZ_APP_DISPLAYNAME=Amanda
+MOZ_APP_BASENAME=Amanda                                                         
+MOZ_APP_PROFILE=amanda                                               
+MOZ_APP_NAME=amanda
+
+MOZ_APP_UA_NAME=Amanda
+MOZ_APP_UA_VERSION=1.0
